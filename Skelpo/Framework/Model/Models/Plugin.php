b9a3320f284d94a1deda90675646b0027d4287e5
Ralph Küpper <ralph.kuepper@skelpo.com>
2015-05-30 11:16:47 +0200

    Changed the Cappuccino annotations.

--- Skelpo/Framework/Model/Models/Plugin.php
+++ Skelpo/Framework/Model/Models/Plugin.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * This file is part of the skelpo framework.
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- * 
+ *
  * @version 1.0.0-alpha
  * @author Ralph Kuepper <ralph.kuepper@skelpo.com>
  * @copyright 2015 Skelpo Inc. www.skelpo.com
@@ -15,15 +16,14 @@ use Skelpo\Framework\Model\Model;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
-use App\Annotations\CPModel;
-
+use App\Annotations\Cappuccino as CP;
 
 /**
- * This class is the model for a Plugin. 
+ * This class is the model for a Plugin.
  *
  * @ORM\Entity
  * @ORM\Table(name="plugins")
- * @CPModel
+ * @CP\Model
  */
 class Plugin extends Model
 {
@@ -33,12 +33,12 @@ class Plugin extends Model
 	 * @ORM\GeneratedValue(strategy="AUTO")
 	 */
 	protected $id;
-	 
+	
 	/**
 	 * @ORM\Column(type="string", length=255, unique=true, nullable=false)
 	 * @Assert\NotBlank
 	 * @Assert\Length(min=3)
-	 * @CPModel(editable=false, list=false, singleView=true)
+	 * @CP\Model(editable=false, list=false, singleView=true)
 	 */
 	protected $slug;
 	
@@ -46,7 +46,7 @@ class Plugin extends Model
 	 * @ORM\Column(type="string", length=255, nullable=false)
 	 * @Assert\NotBlank
 	 * @Assert\Length(min=3)
-	 * @CPModel(editable=false, list=true, singleView=true)
+	 * @CP\Model(editable=false, list=true, singleView=true)
 	 */
 	protected $title;
 	
@@ -54,33 +54,25 @@ class Plugin extends Model
 	 * @ORM\Column(type="string", length=255, nullable=false)
 	 * @Assert\NotBlank
 	 * @Assert\Length(min=3)
-	 * @CPModel(editable=false, list=false, singleView=true)
+	 * @CP\Model(editable=false, list=false, singleView=true)
 	 */
 	protected $name;
-	 
+	
 	/**
 	 * @ORM\Column(type="string", length=255, nullable=false)
-	 * @CPModel(editable=false, list=true, singleView=false)
-	 * 
+	 * @CP\Model(editable=false, list=true, singleView=false)
 	 */
 	protected $author;
 	/**
 	 * @ORM\Column(type="string", length=10, nullable=false)
-	 * 
 	 */
 	protected $version;
-	 /**
+	/**
 	 * @ORM\Column(type="smallint", length=1, nullable=false)
-	 * 
 	 */
 	protected $active;
-	 
-	
+
 	public function __construct()
 	{
-	    
 	}
-	
-	
-	    
 }
--- Skelpo/Framework/Model/Models/Theme.php
+++ Skelpo/Framework/Model/Models/Theme.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * This file is part of the skelpo framework.
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- * 
+ *
  * @version 1.0.0-alpha
  * @author Ralph Kuepper <ralph.kuepper@skelpo.com>
  * @copyright 2015 Skelpo Inc. www.skelpo.com
@@ -16,9 +17,8 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
-
 /**
- * This class is the model for a theme. 
+ * This class is the model for a theme.
  *
  * @ORM\Entity
  * @ORM\Table(name="themes")
@@ -31,7 +31,7 @@ class Theme extends Model
 	 * @ORM\GeneratedValue(strategy="AUTO")
 	 */
 	protected $id;
-	 
+	
 	/**
 	 * @ORM\Column(type="string", length=255, unique=true, nullable=false)
 	 * @Assert\NotBlank
@@ -52,29 +52,21 @@ class Theme extends Model
 	 * @Assert\Length(min=3)
 	 */
 	protected $name;
-	 
+	
 	/**
 	 * @ORM\Column(type="string", length=255, nullable=false)
-	 * 
 	 */
 	protected $author;
 	/**
 	 * @ORM\Column(type="string", length=10, nullable=false)
-	 * 
 	 */
 	protected $version;
-	 /**
+	/**
 	 * @ORM\Column(type="smallint", length=1, nullable=false)
-	 * 
 	 */
 	protected $active;
-	 
-	
+
 	public function __construct()
 	{
-	    
 	}
-	
-	
-	    
 }
